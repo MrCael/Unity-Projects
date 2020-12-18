f8e1b5f737b89dba895a6656b4e0cd5e65e85dfc
MrCael <cael.e988@stu.nebo.edu>
2020-12-17 23:02:22 -0700

    Grid Generation and Player Movement

---
+++ Gun Game/Assets/Scripts/GridManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public float x_start, y_start;
+    public int columnLength, rowLength;
+    public float x_space, y_space;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < columnLength * rowLength; i++)
+        {
+            Instantiate(prefab, new Vector3(x_start + (x_space * (i % columnLength)), -y_start + (y_space * (i / columnLength))), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Gun Game/Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, new Vector3(-4, -4, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
